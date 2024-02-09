c5ec70e073d8b398dcf8c0cbcd15b6e1c3b8d234
Billy Baney <bbaney0315@gmail.com>
2024-02-09 11:53:21 -0600

    asks for user input on a board size, to change the board between 3 different sizes

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const container = document.getElementById('container')
 const resetBtn = document.createElement('button')
+const button1 = document.createElement('button')
+const button2 = document.createElement('button')
+const button3 = document.createElement('button')
+const buttonDiv = document.createElement('div')
 
 resetBtn.innerHTML = "Reset Board"
 resetBtn.classList.add("reset")
 
+button1.innerHTML = "16 X 4"
+button2.innerHTML = "16 X 8"
+button3.innerHTML = "16 X 16"
+buttonDiv.classList.add('btn-div')
 
+let count = prompt("How big would you like the board? 16x4, 16x8, 16x16: ")
 
-let count = 256
+if (count === '16x4') {
+    count = 64
+} else if (count === '16x8') {
+    count = 128
+} else if (count === '16x16') {
+    count = 264
+} else {
+    window.location.reload()
+}
 
 for (let i = 0; i < count; i++) {
     let div = document.createElement('div')
@@ -17,6 +34,10 @@ for (let i = 0; i < count; i++) {
     })
 }
 
+container.appendChild(buttonDiv)
+buttonDiv.appendChild(button1)
+buttonDiv.appendChild(button2)
+buttonDiv.appendChild(button3)
 container.appendChild(resetBtn)
 
 resetBtn.addEventListener('click', () => {
